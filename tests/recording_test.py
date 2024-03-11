019c744e1018ce0b1741cc20d4a76cee8aa27621
Harald Mack <harald.mack@uni-heidelberg.de>
2024-03-11 16:42:48 +0100

    add testrunner script, use unittest for testin in cli

--- tests/recording_test.py
+++ tests/recording_test.py
@@ -1,16 +1,16 @@
 import unittest
+import time  # simulate runtime
+
 
 class DummyTest(unittest.TestCase):
-
 
     def setUp(self):
         self.x = 3
 
-
     def tearDown(self):
         pass
 
-
     def test_dummy(self):
+        time.sleep(2)
         y = self.x + 4
         self.assertEqual(y, 7)
---
+++ tests/testrunner.py
@@ -0,0 +1,6 @@
+import unittest
+
+import recording_test as rt
+
+if __name__ == "__main__":
+    unittest.main()
