3e9ff32aec4bd053ddd43ab8d1df391d356c28c7
Harald Mack <harald.mack@uni-heidelberg.de>
2024-03-11 17:23:18 +0100

    add model files using lsf

---
+++ models/google_perch/model.py
@@ -0,0 +1,10 @@
+class Model:
+    """_summary_"""
+
+    def __init__(self):
+        ## load the model and the ready it for work
+        pass
+
+    def predict(self, data):
+        ## create an actual analysis of some data
+        pass
