b4449f240200c1ba6ecb0073d7d0f348523e1b49
Harald Mack <harald.mack@uni-heidelberg.de>
2024-03-11 15:28:59 +0100

    add init and dummy test

---
+++ tests/recording_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+class DummyTest(unittest.TestCase):
+
+
+    def setUp(self):
+        self.x = 3
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_dummy(self):
+        y = self.x + 4
+        self.assertEqual(y, 4)
