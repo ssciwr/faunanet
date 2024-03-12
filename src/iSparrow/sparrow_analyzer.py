af5e8d931216bd288d00b7cbf2621baede162af1
Harald Mack <harald.mack@protonmail.com>
2024-03-12 17:11:14 +0100

    finish provisionary implementatoin of SparrowAnalyzer and SparrowRecording, add tests for custom recording and analyzer with custom model

--- src/iSparrow/sparrow_analyzer.py
+++ src/iSparrow/sparrow_analyzer.py
@@ -1,8 +1,15 @@
-from birdnetlib.analyzer import Analyzer
-import numpy as np 
+from birdnetlib.analyzer import Analyzer, AnalyzerConfigurationError
+import numpy as np
+from pathlib import Path
 
 
-class SparrowAnalyzer(Analyzer): 
+try:
+    import tflite_runtime.interpreter as tflite
+except:
+    from tensorflow import lite as tflite
+
+
+class SparrowAnalyzer(Analyzer):
     """
     SparrowAnalyzer Specialization of birdnetlib.Analyzer to get the custom model to work
 
@@ -16,9 +23,69 @@ class SparrowAnalyzer(Analyzer):
 
         _extended_summary_
         """
-        self.apply_sigmoid = cfg['Model']['apply_sigmoid']
-        self.sigmoid_sensitivity = cfg['Model']['sigmoid_sensitivity']
-        super().__init__()
+        self.apply_sigmoid = cfg["Model"]["apply_sigmoid"] if "" in cfg["Model"] else True
+
+        self.sigmoid_sensitivity = cfg["Model"]["sigmoid_sensitivity"] if "" in cfg["Model"] else 1.0
+
+        self.num_threads = cfg["Model"]["num_threads"] if "num_threads" in cfg["Model"] else 1
+
+        # README:  the path treatment below is a temporary solution that will change again once the sparrow model itself is implemented.
+        #          it will be generalized to arbitrary models and then be put there. This will also do away with the custom <-> default distinction
+        custom_species_list_path = cfg["Model"]["custom_species_list_path"] if "custom_species_list_path" in cfg["Model"] else None
+
+        custom_species_list = cfg["Model"]["custom_species_list"] if "custom_species_list" in cfg["Model"] else None
+
+        if Path(custom_species_list_path).exists() is False:
+            raise AnalyzerConfigurationError("custom species list path does not exist")
+
+        if Path(custom_species_list).exists() is False:
+            raise AnalyzerConfigurationError("custom species list file does not exist")
+
+        if "path" not in cfg["Model"] or cfg["Model"]["path"] != "birdnet_default":
+            if (Path(cfg["Model"]["model_dir"]) / Path(cfg["Model"]["model_name"]) / Path("model.tflite")).exists() is False:
+                raise AnalyzerConfigurationError("model file does not exist")
+
+            if (Path(cfg["Model"]["model_dir"]) / Path(cfg["Model"]["model_name"]) / Path("labels.txt")).exists() is False:
+                raise AnalyzerConfigurationError("labels file does not exist")
+
+            classifier_model_path = str(Path(cfg["Model"]["model_dir"]) / Path(cfg["Model"]["model_name"]) / Path("model.tflite"))
+
+            classifier_labels_path = str(Path(cfg["Model"]["model_dir"]) / Path(cfg["Model"]["model_name"]) / Path("labels.txt"))
+
+        else:
+            if (Path(cfg["Model"]["model_dir"]) / Path(cfg["Model"]["model_name"]) / Path("model.tflite")).exists() is False:
+                raise AnalyzerConfigurationError("default model file does not exist")
+
+            if (Path(cfg["Model"]["model_dir"]) / Path(cfg["Model"]["model_name"]) / Path("labels.txt")).exists() is False:
+                raise AnalyzerConfigurationError("default labels file does not exist")
+
+            self.default_model_path = Path(cfg["Model"]["model_dir"]) / Path(cfg["Model"]["model_name"]) / Path("model.tflite")
+
+            self.default_labels_path = Path(cfg["Model"]["model_dir"]) / Path(cfg["Model"]["model_name"]) / Path("labels.txt")
+
+        super().__init__(custom_species_list_path=custom_species_list_path,
+                         custom_species_list=custom_species_list,
+                         classifier_model_path=classifier_model_path,
+                         classifier_labels_path=classifier_labels_path)
+
+    def load_model(self):
+        """
+        load_model Override of the base class load model to get the correct paths and make sure that we have multithreading
+        """
+        self.interpreter = tflite.Interpreter(model_path=str(self.default_model_path), num_threads=self.num_threads)
+        self.interpreter.allocate_tensors()
+
+        # Get input and output tensors.
+        self.input_details = self.interpreter.get_input_details()
+        self.output_details = self.interpreter.get_output_details()
+
+        # Get classification output or feature embeddings
+        if self.use_custom_classifier:
+            self.output_layer_index = self.output_details[0]["index"] - 1
+        else:
+            self.output_layer_index = self.output_details[0]["index"]
+
+        print("Model loaded.")
 
     def get_embeddings(self, data):
         """
@@ -32,9 +99,8 @@ class SparrowAnalyzer(Analyzer):
         Returns:
             _type_: _description_
         """
-        # this is what the "embeddings" function in the normal BirdNET library does. 
-        # I'm not quite sure why this is important and what happens?
-        # get embeddings data --> why does this work? what does it do? 
+        # this is what the "embeddings" function in the normal BirdNET library does.
+        # gets the feature embeddings from the
         self.interpreter.resize_tensor_input(self.input_layer_index, [len(data), *data[0].shape])
         self.interpreter.allocate_tensors()
 
@@ -57,9 +123,10 @@ class SparrowAnalyzer(Analyzer):
         Returns:
             _type_: _description_
         """
-        # overrides the predict_with_custom_classifier in the 'Analyzer' class 
-        # of birdnetlib with what the BirdNET-Analyzer system does. 
-        # why does this work? no idea... 
+        # overrides the predict_with_custom_classifier in the 'Analyzer' class
+        # of birdnetlib with what the BirdNET-Analyzer system does.
+        # FIXME: will be replaced in a later PR with a more concise implementation
+
         data = np.array([sample], dtype="float32")
 
         input_details = self.custom_interpreter.get_input_details()
@@ -67,7 +134,7 @@ class SparrowAnalyzer(Analyzer):
         input_size = input_details[0]["shape"][-1]
 
         feature_vector = self.get_embeddings(data) if input_size != 144000 else data
- 
+
         self.custom_interpreter.resize_tensor_input(
             self.custom_input_layer_index, [len(feature_vector), *feature_vector[0].shape]
         )
--- tests/recording_test.py
+++ tests/recording_test.py
@@ -1,34 +1,60 @@
+from src.iSparrow import sparrow_analyzer as spa
+from src.iSparrow import sparrow_recording as spc
 import sys
 import yaml
 import pytest
 import numpy as np
 from numpy.testing import assert_array_equal
 import importlib
-
+import pandas as pd
+from pandas.testing import assert_frame_equal
 from pathlib import Path
 
 sys.path.append('../src/iSparrow')
-from src.iSparrow import sparrow_recording as spc
-from src.iSparrow import sparrow_analyzer as spa
 
 
 @pytest.fixture
-def recorded_data():
+def recorded_data_for_construction():
+
     tests = Path(__file__).resolve().parent
-    home = tests.parent
+
     with open(tests / Path("example") / "test.yml", "r") as file:
         cfg = yaml.safe_load(file)
 
-    pp = importlib.import_module('models.' + cfg["Analyzer"]["Model"]["path"]+'.preprocessor')
+    pp = importlib.import_module('models.' + cfg["Analyzer"]["Model"]["model_name"] + '.preprocessor')
 
     preprocessor = pp.Preprocessor(cfg["Data"]["Preprocessor"])
+
     analyzer = spa.SparrowAnalyzer(cfg["Analyzer"])
-    recording = spc.SparrowRecording(analyzer, preprocessor, tests / "example/soundscape.wav")
-    return recording, tests, home
+
+    recording = spc.SparrowRecording(analyzer, preprocessor, tests / "example/soundscape.wav", min_conf=0.25,)
+
+    return recording, tests
 
 
-def test_recording_construction(recorded_data):
-    recording, testpath, homepath = recorded_data
+@pytest.fixture
+def recorded_data_for_analysis():
+
+    tests = Path(__file__).resolve().parent
+
+    with open(tests / Path("example") / "test.yml", "r") as file:
+        cfg = yaml.safe_load(file)
+
+    pp = importlib.import_module('models.' + cfg["Analyzer"]["Model"]["model_name"] + '.preprocessor')
+
+    preprocessor = pp.Preprocessor(cfg["Data"]["Preprocessor"])
+
+    analyzer = spa.SparrowAnalyzer(cfg["Analyzer"])
+
+    recording = spc.SparrowRecording(analyzer, preprocessor, tests / "example/soundscape.wav", min_conf=0.25,)
+
+    analysis_results = pd.read_csv(tests / Path("example") / Path("custom_results.csv"))
+
+    return recording, analysis_results
+
+
+def test_recording_construction(recorded_data_for_construction):
+    recording, testpath = recorded_data_for_construction
 
     assert recording.path == testpath / Path("example/soundscape.wav")
     assert recording.filename == "soundscape.wav"
@@ -41,21 +67,25 @@ def test_recording_construction(recorded_data):
     assert recording.processor.resample_type == 'kaiser_fast'
     assert recording.processor.duration == 0  # unknown at this point, hence zero
     assert recording.processor.actual_sampling_rate == pytest.approx(0.)  # unknown at this point, hence zero
+    assert recording.minimum_confidence == pytest.approx(0.25)
 
 
-def test_recording_reading(recorded_data):
+def test_recording_reading(recorded_data_for_construction):
 
-    recording, testpath, homepath = recorded_data
+    recording, testpath = recorded_data_for_construction
     data = recording.processor.read_audio_data(testpath / Path("example/soundscape.wav"))
 
     assert len(data) == pytest.approx(recording.processor.actual_sampling_rate * recording.processor.duration)
     assert recording.processor.actual_sampling_rate == 48000
     assert recording.processor.duration == pytest.approx(120.0)
 
+    
 
-def test_recording_processing(recorded_data):
 
-    recording, testpath, homepath = recorded_data
+
+def test_recording_processing(recorded_data_for_construction):
+
+    recording, testpath = recorded_data_for_construction
     # use trimmed audio file that's not a multiple of 3s in length
 
     data = recording.processor.read_audio_data(testpath / Path("example/trimmed.wav"))
@@ -69,5 +99,13 @@ def test_recording_processing(recorded_data):
     assert_array_equal(chunks[-1][72000::], np.zeros(72000))
 
 
-def test_analysis(recorded_data):
-    assert 3 == 3
+def test_analysis(recorded_data_for_analysis):
+
+    recording, expected_analysis_results = recorded_data_for_analysis
+
+    recording.analyze()
+
+    # make a dataframe and sort it the same way as the expected data, with an index ranging from 0:len(df)-1
+    df = pd.DataFrame(recording.detections).sort_values(by="confidence", ascending=False).reset_index().drop("index", axis=1)
+
+    assert_frame_equal(df, expected_analysis_results)
