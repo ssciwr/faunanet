b12e4f3491a648a655248b5e5be59b99f57e8bb9
Harald Mack <harald.mack@protonmail.com>
2024-03-12 10:57:05 +0100

    fix error in gitignore, re-add pytests, delete main.py

--- tests/recording_test.py
+++ tests/recording_test.py
@@ -1,16 +1,21 @@
-import unittest
+import pytest
 import time  # simulate runtime
 
 
-class DummyTest(unittest.TestCase):
-
-    def setUp(self):
-        self.x = 3
-
-    def tearDown(self):
-        pass
-
-    def test_dummy(self):
+class Dummy: 
+    def __init__(self): 
+        self.x = 3  
+    
+    def test_dummy(self): 
         time.sleep(2)
-        y = self.x + 4
-        self.assertEqual(y, 7)
+        return self.x + 4
+
+
+@pytest.fixture 
+def dummyfixture(): 
+    return Dummy()
+
+
+def test_dummy(dummyfixture): 
+    y = dummyfixture.test_dummy() 
+    assert y == 7
