7f6804a8c50945c7606d6a6b52323f69a2510ad2
Harald Mack <harald.mack@protonmail.com>
2024-03-15 14:21:32 +0100

    start to write perprocessor for the google model

--- models/google_perch/preprocessor.py
+++ models/google_perch/preprocessor.py
@@ -13,19 +13,26 @@ class Preprocessor(ppb.PreprocessorBase):
 
     """
 
-    def __init__(self, cfg: dict):
+    def __init__(
+        self,
+        sample_rate: int = 48000,
+        overlap: float = 0.0,
+        sample_secs: int = 3.0,
+        resample_type: str = "kaiser_fast",
+    ):
         """
-        __init__ Create a new data processor.
-
-        Reads in audio data and processes them, ready to be analyzed by the analyzer of bridnetlib.
+        __init__ Construct a new preprocesssor for custom birdnet classifiers from given parameters, and use defaults for the ones not present.
 
         Args:
-            cfg (dict): config node for preprocessing
+            sample_rate (int, optional): The sample rate used to resample the read audio file. Defaults to 48000.
+            overlap (float, optional): Overlap between chunks to be analyzed. Defaults to 0.0.
+            sample_secs (int, optional): Length of chunks to be analyzed at once. Defaults to 3.0.
+            resample_type (str, optional): Resampling method used when reading from file. Defaults to "kaiser_fast".
         """
-        self.sample_rate = cfg["sample_rate"]
-        self.overlap = cfg["overlap"]
-        self.sample_secs = cfg["sample_secs"]
-        self.resample_type = cfg["resample_type"]
+        self.sample_rate = sample_rate
+        self.overlap = overlap
+        self.sample_secs = sample_secs
+        self.resample_type = resample_type
         self.duration = 0
         self.actual_sampling_rate = 0
         super().__init__("google_perch")
@@ -45,7 +52,28 @@ class Preprocessor(ppb.PreprocessorBase):
 
         print("read audio file custom")
 
-        return np.array([])
+        try:
+            data, rate = librosa.load(
+                path, sr=self.sample_rate, mono=True, res_type=self.resample_type
+            )
+
+            self.duration = librosa.get_duration(y=data, sr=self.sample_rate)
+            self.actual_sampling_rate = rate
+
+        except audioread.exceptions.NoBackendError as e:
+            print(e)
+            raise AudioFormatError("Audio format could not be opened.")
+        except FileNotFoundError as e:
+            print(e)
+            raise e
+        except Exception as e:
+            print(e)
+            raise AudioFormatError("Generic audio read error occurred from librosa.")
+
+        if self.actual_sampling_rate != self.sample_rate:
+            raise RuntimeError(
+                "Error, sampling rate from resampling and desired sampling rate don't match"
+            )
 
     def process_audio_data(self, rawdata: np.ndarray) -> list:
         """
