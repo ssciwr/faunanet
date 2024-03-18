e7f00a9cbb79c052b61430cc53c2c1158e2735e0
Harald Mack <harald.mack@protonmail.com>
2024-03-18 09:07:44 +0100

    add basic constructor to model base class

--- src/iSparrow/sparrow_model_base.py
+++ src/iSparrow/sparrow_model_base.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
-
 import numpy as np
+from pathlib import Path
 
 
 class ModelBase(ABC):
@@ -10,6 +10,23 @@ class ModelBase(ABC):
     Args:
         ABC (_type_): _description_
     """
+
+    def __init__(self, model_path: str, labels_path: str, num_threads: int):
+        self.num_threads = num_threads
+
+        if Path(model_path).exists() is False:
+            raise FileNotFoundError(f"No model file at {model_path}")
+
+        if Path(labels_path).exists() is False:
+            raise FileNotFoundError(f"No labels file at {labels_path}")
+
+        self.model_path = model_path
+
+        self.labels_path = labels_path
+
+        self.load_model()
+
+        self.load_labels()
 
     @abstractmethod
     def load_labels(self):
