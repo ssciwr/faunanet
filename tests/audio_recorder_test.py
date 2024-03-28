c123e7398ed705b9ef0fdfda6ec06a873797ceda
Harald Mack <harald.mack@protonmail.com>
2024-03-28 08:37:50 +0100

    try to get audio tests to work in pipeline

--- tests/audio_recorder_test.py
+++ tests/audio_recorder_test.py
@@ -10,7 +10,8 @@ def test_audio_recorder_creation(folders, audio_recorder_fx):
 
     p = pyaudio.PyAudio()
     num_devices = p.get_device_count()
-    print("checking audio devices")
+    print("checking audio devices", num_devices)
+
     for i in range(num_devices):
         device_info = p.get_device_info_by_index(i)
         print(f"Device {i}: {device_info['name']}")
