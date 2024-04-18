baceebe1cb9b24fa1dd81cc7a8e149deff0ba2d8
Harald Mack <harald.mack@protonmail.com>
2024-04-18 07:01:02 +0200

    add cli skeleton

---
+++ tests/cli_test.py
@@ -0,0 +1,31 @@
+import iSparrow.cli as cli
+from click.testing import CliRunner
+from pathlib import Path
+
+
+def test_cli_install():
+    pass
+
+
+def test_cli_show():
+    pass
+
+
+def test_cli_start():
+    pass
+
+
+def test_cli_stop():
+    pass
+
+
+def test_cli_check():
+    pass
+
+
+def test_cli_change_analyzer():
+    pass
+
+
+def test_cli_clean_up():
+    pass
