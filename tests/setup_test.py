5543e8312af605f31e4bb77fd1d397551cb0aa60
Harald Mack <harald.mack@protonmail.com>
2024-05-15 20:32:27 +0200

    add folder existence check to setup test

--- tests/setup_test.py
+++ tests/setup_test.py
@@ -48,12 +48,16 @@ def clean_up_test_installation():
     cfg = sps.utils.read_yaml(
         Path(__file__).parent / "test_install_config" / "install.yml"
     )
-
     for _, path in cfg["Directories"].items():
-        if (Path(path).expanduser().exists()): 
+        if Path(path).expanduser().exists():
             shutil.rmtree(Path(path).expanduser(), ignore_errors=True)
-    shutil.rmtree(Path(user_config_dir()) / "iSparrow_tests", )
-    shutil.rmtree(Path(user_cache_dir()) / "iSparrow_tests", )
+
+    if (Path(user_config_dir()) / "iSparrow_tests").exists():
+        shutil.rmtree(Path(user_config_dir()) / "iSparrow_tests", ignore_errors=True)
+
+    if (Path(user_cache_dir()) / "iSparrow_tests").exists():
+        shutil.rmtree(Path(user_cache_dir()) / "iSparrow_tests", ignore_errors=True)
+
 
 def test_make_directories(temp_dir, cleanup_after_test):
     base_cfg_dirs = {
