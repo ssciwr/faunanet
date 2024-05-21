422690172320bf19a6d8b84fba81e76f19cc7f95
Harald Mack <harald.mack@protonmail.com>
2024-05-21 15:01:36 +0200

    remove old theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -55,9 +55,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = "nature"
 html_theme = "sphinx_rtd_theme"
-
 html_theme_options = {
     "sidebarwidth": "40em",
 }
