11374ffda88b7b87c14cf7faeca777ecce71b895
Harald Mack <harald.mack@protonmail.com>
2024-05-27 09:13:06 +0200

    add version

--- src/iSparrow/__init__.py
+++ src/iSparrow/__init__.py
@@ -4,3 +4,6 @@ from .preprocessor_base import PreprocessorBase
 from .species_predictor import SpeciesList
 from .species_predictor import SpeciesPredictorBase
 from .sparrow_watcher import SparrowWatcher
+
+__all__ = ["ModelBase", "SparrowRecording", "PreprocessorBase", "SpeciesList", "SpeciesPredictorBase", "SparrowWatcher", "__version__"]
+__version__ = "0.0.1"
